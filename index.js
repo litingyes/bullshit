bfae6fcfbe85de4ee69b38a251e892f0fd9f9a96
liting <2972188187@qq.com>
2022-09-16 20:57:10 +0800

    chore: add license

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-import { readFileSync } from 'fs'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 import { generate } from './lib/generator.js'
 import { createRandomPicker } from './lib/random.js'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-function loadCorpus(src) {
-    const path = resolve(__dirname, src)
-    const data = readFileSync(path, { encoding: 'utf-8' })
-    return JSON.parse(data)
-}
+import { loadCorpus, savaCorpus } from './lib/corpus.js'
 
 const corpus = loadCorpus('corpus/data.json')
 
@@ -18,4 +8,4 @@ const pickTitle = createRandomPicker(corpus.title)
 const title = pickTitle()
 
 const article = generate(title, { corpus })
-console.log(`${title}\n\n    ${article.join('\n  ')}`)
+savaCorpus(title, article)
